4261a873c2d837fee20fddf0174912ee848614a5
DESKTOP-R71C5UD\fitz <zjtxchen@gmail.com>
2021-04-23 09:14:18 +0800

    基本权限框架完成 能够基于Role和Permission鉴权

--- src/test/java/com/bin/SecurityApplicationTests.java
+++ src/test/java/com/bin/SecurityApplicationTests.java
@@ -1,13 +1,79 @@
 package com.bin;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Method;
 
 @SpringBootTest
 class SecurityApplicationTests {
 
+    @Autowired
+    ConfigurableApplicationContext run;
+
+    /**
+     * BCrypt加盐加密
+     */
     @Test
     void contextLoads() {
+        BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+        //注：底层已经加盐了！！
+        String encode = passwordEncoder.encode("123456");
+        System.err.println(encode);
+    }
+
+    /**
+     * 获取所有方法url
+     * @param run
+     */
+    @Test
+    void getAllUrl(ConfigurableApplicationContext run) {
+
+        //LOGGER.info("getUrlMethod is called.");
+
+        StringBuilder sbSql = new StringBuilder();
+        sbSql.append("INSERT INTO interfacelist (url,name,type)values");
+
+        //获取restcontroller注解的类名
+        String[] beanNamesForAnnotation = run.getBeanNamesForAnnotation(RestController.class);
+
+        //获取类对象
+        for (String str : beanNamesForAnnotation) {
+            Object bean = run.getBean(str);
+            Class<?> forName = bean.getClass();
+            System.out.println(forName.getName());
+
+            //获取requestmapping注解的类
+            RequestMapping declaredAnnotation = forName.getAnnotation(RequestMapping.class);
+
+            String url_path = "";
+            if (declaredAnnotation != null) {
+                String[] value = (declaredAnnotation.value());
+                //获取类的url路径
+                url_path = value[0];
+                for (Method method : forName.getDeclaredMethods()) {
+                    RequestMapping annotation2 = method.getAnnotation(RequestMapping.class);
+                    if (annotation2 != null) {
+                        url_path += annotation2.value()[0];
+                        System.out.println("方法路径" + url_path + "方法名" + method.getName());
+                        sbSql.append("('").append(url_path).append("','").append(method.getName()).append("','").append("api'),");
+                    }
+                    url_path = value[0];
+                }
+            }
+        }
+
+        //删除字符串sbCols最后一个逗号
+        sbSql.deleteCharAt(sbSql.length() - 1);
+
+        System.out.println(sbSql);
+
+        System.out.println(sbSql.toString());
     }
 
 }
